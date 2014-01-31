bd25d862809edd172964ca5b161639bcf8944586
Joe Crawley <joe.crawley10@gmail.com>
2014-01-31 03:24:30 -0800

    added files from work, will be live soon

---
+++ tweet-grabber.js
@@ -0,0 +1,108 @@
+//$(document).ready(function(){
+	var fb = new Firebase("https://blazing-fire-897.firebaseio.com/tweets");
+
+	var statuses = [];
+
+	var html = [];
+
+
+
+	$("#tweet-box").css('height', $(window).height());
+	$("#text-box").css('width', $(window).width()/2);
+	$("#input").css('width', $(window).width()/2);
+	$("#input").css('top', $(window).height()/2);
+	$(".alert").hide();
+	$("#loading").hide();
+
+	fb.on('value', function(nameSnapshot) {
+		$.each(nameSnapshot.val(), function( index, value ) {
+			value.statuses.forEach(function(element,index,array){
+				statuses.push(element);
+			});
+		});
+		console.log(statuses);
+	  
+	  statuses.forEach(function(element,index,array){
+			$.ajax({
+
+				url: 'https://api.twitter.com/1/statuses/oembed.json?id='+ element.id_str,
+				type: 'GET',
+				crossDomain: true,
+				dataType: 'jsonp',
+				success: function(response) { 
+					$(response.html).attr('id', element.id_str);
+					element.html = response.html;
+
+					//element.html = element.html.splice(0,"\u003Cblockquote".length) + " id = \' " + element.id_str + " \' " + element.html.splice("\u003Cblockquote".length, element.html.length);
+					
+					$('#tweet-box').append(element.html);
+					$($('.twitter-twitter-tweet.twitter-tweet-rendered')[index]).attr('id', element.id_str);
+					//console.log(pushed);
+				},
+				error: function() {   },
+			});
+		});
+	});
+
+
+
+	$('#input').keyup(function(){
+		$(".jumbotron").hide();
+		
+		var search = $('#input').val();
+		var count = 0;
+		$('#text-box').css('width', $("#tweet-box").width()/2 + "px");
+		$('#tweet-box').show(700);
+
+
+		var allFalse = true;
+
+		statuses.forEach(function(element,index,array){
+			var text = element.text;
+			element.show = (text.search(search) > -1);
+			
+			if(element.show){
+				allFalse = false;
+				
+			}
+			
+		});
+		$("iframe").load(function(){
+					
+			//$("#loading").hide();
+			console.log(ran);
+
+		});
+
+		statuses.forEach(function(element,index,array){
+			
+
+			if(allFalse){
+				$(".alert").show(500);
+
+			}
+	
+			
+
+			else{
+				$(".alert").hide(500);
+				element.show ? $("#twitter-widget-" + index).show() : $("#twitter-widget-" + index).hide();
+
+				
+
+			}
+
+			
+			
+		});
+
+		
+			
+			
+			
+
+			
+
+	});
+
+//});
---
+++ tweet-grabber.php
@@ -0,0 +1,48 @@
+<?php
+
+/** called in a cron job on my.cs.lmu.edu/~jcrawley/~tweet-grabber.php**/
+ini_set('display_errors', 1);
+require_once('TwitterAPIExchange.php');
+require_once('firebaseLib.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "205910382-BDOrmoghDMVFlxPjxmXhuclmmSW0frzjitWNTQ16",
+    'oauth_access_token_secret' => "VCqTiWjOWEECxWHTTjRwYMCs1uQor0dTNMBQFqkmI9q3k",
+    'consumer_key' => "hS0FRlszeVrFp6O1szrYAQ",
+    'consumer_secret' => "nUbPeJmyAQMmPFGBTYG14IIDXbgPrq42h6P3pHio"
+);
+
+
+
+/** Perform a GET request and echo the response **/
+/** Note: Set the GET field BEFORE calling buildOauth(); **/
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$getfield = '?q=%23inspiration%20OR%20%23inspirational&count=1000';
+$requestMethod = 'GET';
+$twitter = new TwitterAPIExchange($settings);
+$tweets =  $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
+
+$url = 'https://blazing-fire-897.firebaseio.com/';
+$token = '0dlcc1uSwJNfkGjvTZJ2znMJlDgaYAq3jwqDiXV6';
+
+$fb = new fireBase($url, $token);
+
+$todoPath = '/tweets';
+
+/*$data = $fb->get('tweets/statuses/0/id_str');
+echo $data;*/
+
+printf("Pushing data from %s\n", $todoPath);
+
+$response = $fb->push($todoPath, json_decode($tweets));
+
+/**printf("Result: %s\n", $response);**/
+sleep(2);
+?>
+
+
+
+
